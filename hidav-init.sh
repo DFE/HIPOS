f0b2a7affdeb8bfbd8632169541c63e294637da2
Dirk Nilius <nilius@Kubuntu-Precision-T3400.(none)>
2011-12-09 12:14:00 +0100

    added external repositories as submodules

--- hidav-init.sh
+++ hidav-init.sh
@@ -3,31 +3,6 @@
 # $Id: hidav-init.sh 86429 2011-12-08 07:43:35Z nilius $
 #
 
-OE_CORE_NAME=oe-core
-OE_CORE_URI=git://git.openembedded.org/openembedded-core
-OE_CORE_BRANCH=master
-OE_CORE_COMMIT_ID=
-
-META_OE_NAME=meta-openembedded
-META_OE_URI=http://cgit.openembedded.org/cgit.cgi/meta-openembedded
-META_OE_BRANCH=master
-META_OE_COMMIT_ID=
-
-META_TI_NAME=meta-texasinstruments
-META_TI_URI=http://git.angstrom-distribution.org/cgi-bin/cgit.cgi/meta-texasinstruments
-META_TI_BRANCH=master
-META_TI_COMMIT_ID=
-
-META_ANGSTROM_NAME=meta-angstrom
-META_ANGSTROM_URI=http://git.angstrom-distribution.org/cgi-bin/cgit.cgi/meta-angstrom
-META_ANGSTROM_BRANCH=master
-META_ANGSTROM_COMMIT_ID=
-
-BITBAKE_NAME=bitbake
-BITBAKE_URI=git://git.openembedded.org/bitbake
-BITBAKE_BRANCH=master
-BITBAKE_COMMIT_ID=
-
 log()
 {
   local TIMESTAMP=`date -u +%Y-%m-%d-%H:%M:%S`
@@ -47,67 +22,12 @@ sanity()
   log "I: linux sanity check"
 
   GIT=`which git`
-  [ -z "${GIT}" ]  && log "E: couldn't find git" && return 1;
-  log "I:  check git: OK"
+  [ -z "${GIT}" ] && log "E: couldn't find git" && return 1;
+  log "I: check git: OK"
 
   export GIT
 
   return 0
-}
-
-get_from_git()
-{
-  local name=$1
-  local uri=$2
-  local branch=$3
-  local commit_id=$4
-
-  log "I: get ${name}"
-
-  local target_dir="${name}"
-
-  if [ -d "${target_dir}" -a -d "${target_dir}/.git" ]; then
-    log "I:  updating ${name}"
-
-    pushd "${target_dir}" > /dev/null
-    # cleanup local tree state
-    "${GIT}" reset --hard HEAD > /dev/null
-    # switch to master
-    "${GIT}" checkout -q master
-    "${GIT}" pull
-    # checkout clean ${branch}
-    if [ -n "${branch}" ]; then
-       log "I:  checking out branch ${branch}"
-      "${GIT}" checkout ${branch}
-    fi
-
-    # checkout specific commit if requested
-    if [ -n "${commit_id}" ]; then
-      log "I:  checking out commit ${commit_id}"
-      "${GIT}" branch -D work
-      "${GIT}" checkout -b work "${commit_id}"
-    fi
-    popd > /dev/null
-  else
-    log "I:  checking out from ${uri}"
-
-    rm -rf "${target_dir}"
-    "${GIT}" clone ${uri} "${target_dir}"
-    pushd "${target_dir}" > /dev/null
-
-     # checkout ${branch}
-    if [ -n "${branch}" ]; then
-      log "I:  checking out branch ${branch}"
-      "${GIT}" checkout ${branch}
-    fi
-
-    # checkout specific commit if requested
-    if [ -n "${commit_id}" ]; then
-      log "I:  checking out commit ${commit_id}"
-      "${GIT}" checkout -b work "${commit_id}"
-    fi
-    popd > /dev/null
-  fi
 }
 
 update_bblayers_conf()
@@ -121,23 +41,17 @@ update_bblayers_conf()
 
   echo "BBLAYERS = \" \\" >> ${BB_LAYER_CONF}
   echo "${BASE}/meta-hidav \\" >> ${BB_LAYER_CONF}
-  echo "${BASE}/${META_TI_NAME} \\" >> ${BB_LAYER_CONF}
-  echo "${BASE}/${META_ANGSTROM_NAME} \\" >> ${BB_LAYER_CONF}
-  echo "${BASE}/${META_OE_NAME}/meta-oe \\" >> ${BB_LAYER_CONF}
-  echo "${BASE}/${OE_CORE_NAME}/meta \\" >> ${BB_LAYER_CONF}
+  echo "${BASE}/meta-texasinstruments \\" >> ${BB_LAYER_CONF}
+  echo "${BASE}/meta-angstrom \\" >> ${BB_LAYER_CONF}
+  echo "${BASE}/meta-openembedded/meta-oe \\" >> ${BB_LAYER_CONF}
+  echo "${BASE}/openembedded-core/meta \\" >> ${BB_LAYER_CONF}
 
   echo "\"" >> ${BB_LAYER_CONF}
 }
 
 init()
 {
-  get_from_git ${OE_CORE_NAME} ${OE_CORE_URI} ${OE_CORE_BRANCH} ${OE_CORE_COMMIT_ID}
-  get_from_git ${META_OE_NAME} ${META_OE_URI} ${META_OE_BRANCH} ${META_OE_COMMIT_ID}
-  get_from_git ${META_TI_NAME} ${META_TI_URI} ${META_TI_BRANCH} ${META_TI_COMMIT_ID}
-  get_from_git ${META_ANGSTROM_NAME} ${META_ANGSTROM_URI} ${META_ANGSTROM_BRANCH} ${META_ANGSTROM_COMMIT_ID}
-  get_from_git ${BITBAKE_NAME} ${BITBAKE_URI} ${BITBAKE_BRANCH} ${BITBAKE_COMMIT_ID}
-
-  BB_LINK="${OE_CORE_NAME}/bitbake"
+  BB_LINK="openembedded-core/bitbake"
 
   if [ ! -h "${BB_LINK}" ]; then
     ln -s "../bitbake" "${BB_LINK}"
@@ -162,12 +76,6 @@ fi
 
 BASE="`pwd`"
 
-sanity
-if [ "$?" -ne "0" ]; then
-  log "E: sanity check failed"
-  exit 1
-fi
-
 init
 if [ "$?" -ne "0" ]; then
   log "E: init failed"
@@ -176,7 +84,7 @@ fi
 
 INIT_SCRIPT="init.sh"
 
-echo "source ${BASE}/${OE_CORE_NAME}/oe-init-build-env ${BASE}/build" > "${INIT_SCRIPT}"
+echo "source ${BASE}/openembedded-core/oe-init-build-env ${BASE}/build" > "${INIT_SCRIPT}"
 chmod +x ${INIT_SCRIPT}
 
 if [ "${SOURCED}" = "1" ]; then

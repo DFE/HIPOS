eaa7fc8fd25da4f9c22a3a1cf0b7712e98e7e6a0
Dirk Nilius <nilius@dresearch-fe.de>
2012-01-17 14:10:51 +0100

    added a script to update external layers (update-layers.sh)
    Signed-off-by: Dirk Nilius <nilius@dresearch-fe.de>

---
+++ update-layers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+update_layer()
+{
+	echo "Updating layer: $1"
+	pushd $1
+	git checkout master
+	git pull
+	popd
+}
+
+update_layer bitbake
+update_layer meta-angstrom
+update_layer meta-openembedded
+update_layer meta-texasinstruments
+update_layer openembedded-core

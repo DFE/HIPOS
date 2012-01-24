0405501d3f1cef6490687b0985c6dd5f32968b6a
Dirk Nilius <nilius@dresearch-fe.de>
2012-01-24 11:52:18 +0100

    hidav: added java layer
    Signed-off-by: Dirk Nilius <nilius@dresearch-fe.de>

--- hidav-init.sh
+++ hidav-init.sh
@@ -56,6 +56,7 @@ update_bblayers_conf()
 
   echo "BBLAYERS = \" \\" >> ${BB_LAYER_CONF}
   echo "${BASE}/meta-hidav \\" >> ${BB_LAYER_CONF}
+  echo "${BASE}/meta-java \\" >> ${BB_LAYER_CONF}
   echo "${BASE}/meta-texasinstruments \\" >> ${BB_LAYER_CONF}
   echo "${BASE}/meta-angstrom \\" >> ${BB_LAYER_CONF}
   echo "${BASE}/meta-openembedded/meta-oe \\" >> ${BB_LAYER_CONF}

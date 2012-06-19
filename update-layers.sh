413fe52a23ccf97835d2cb557cd4b6a8f63fc82d
Thilo Fromm <github@thilo-fromm.de>
2012-06-19 14:19:24 +0200

    layers: update to current
    This patch updates the OE laers to their current versions. Since
    mtd-utils 1.4.9 is not available anymore in oe-core the HidaV
    layer now extends the 1.5.0 recipe.
    
    Signed-off-by: Thilo Fromm <github@thilo-fromm.de>

--- update-layers.sh
+++ update-layers.sh
@@ -11,7 +11,8 @@ update_layer()
 	popd
 }
 
-update_layer bitbake
+# tfm: newer bitbake currently breaks the build w/ error messages about FILESPATH.
+# update_layer bitbake
 update_layer meta-angstrom
 update_layer meta-openembedded
 update_layer meta-texasinstruments

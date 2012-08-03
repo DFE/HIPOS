63117f67d5fe388bab158237b8344d514462e949
Erik Bernoth <erik.bernoth@googlemail.com>
2012-08-03 13:47:52 +0200

    test_all: :retab
    Signed-off-by: Erik Bernoth <erik.bernoth@googlemail.com>

--- hidav-testing/integration/test_all.py
+++ hidav-testing/integration/test_all.py
@@ -21,50 +21,50 @@ from test_webdav import TestWebDAV
 from test_nfs import TestNFS
 
 class update:
-	dev = device.Device( devtype = "hidav" )
+    dev = device.Device( devtype = "hidav" )
 
-        print "Waiting for Networking to come up..."
-        max_wait=120
-        while not dev.conn.has_networking():
-	        time.sleep(1)
-	        print ("wait {0}s".format(max_wait))
-	        max_wait -= 1
-        	if max_wait == 0:
-        		break
-	print "Update: package index"
-	dev.update_package_index()
-        print "Connecting to device..."
-        print "Firmware version: %s" % dev.firmware_version
+    print "Waiting for Networking to come up..."
+    max_wait=120
+    while not dev.conn.has_networking():
+        time.sleep(1)
+        print ("wait {0}s".format(max_wait))
+        max_wait -= 1
+        if max_wait == 0:
+            break
+    print "Update: package index"
+    dev.update_package_index()
+    print "Connecting to device..."
+    print "Firmware version: %s" % dev.firmware_version
 
-        print "Boot config:"
-        print "  Kernel:      /dev/mtd%s" % dev.bootconfig["kernel"]
-        print "  rootfs: /dev/blockdev%s" % dev.bootconfig["rootfs"]
-        print "  epoch :             #%s" % dev.bootconfig["epoch"]
-	print "Update: rootfs"
-	dev.install_package( "hydraip-image-pkg" )
-        print "Boot config:"
-        print "  Kernel:      /dev/mtd%s" % dev.bootconfig["kernel"]
-        print "  rootfs: /dev/blockdev%s" % dev.bootconfig["rootfs"]
-        print "  epoch :             #%s" % dev.bootconfig["epoch"]
-	print "Update: kernel"
-	dev.install_package( "kernel-image-2.6.37" )
-        print "Boot config:"
-        print "  Kernel:      /dev/mtd%s" % dev.bootconfig["kernel"]
-        print "  rootfs: /dev/blockdev%s" % dev.bootconfig["rootfs"]
-        print "  epoch :             #%s" % dev.bootconfig["epoch"]
-	print "reboot ..."
-	dev.conn._serial.reboot( sync=True )
-		
+    print "Boot config:"
+    print "  Kernel:      /dev/mtd%s" % dev.bootconfig["kernel"]
+    print "  rootfs: /dev/blockdev%s" % dev.bootconfig["rootfs"]
+    print "  epoch :             #%s" % dev.bootconfig["epoch"]
+    print "Update: rootfs"
+    dev.install_package( "hydraip-image-pkg" )
+    print "Boot config:"
+    print "  Kernel:      /dev/mtd%s" % dev.bootconfig["kernel"]
+    print "  rootfs: /dev/blockdev%s" % dev.bootconfig["rootfs"]
+    print "  epoch :             #%s" % dev.bootconfig["epoch"]
+    print "Update: kernel"
+    dev.install_package( "kernel-image-2.6.37" )
+    print "Boot config:"
+    print "  Kernel:      /dev/mtd%s" % dev.bootconfig["kernel"]
+    print "  rootfs: /dev/blockdev%s" % dev.bootconfig["rootfs"]
+    print "  epoch :             #%s" % dev.bootconfig["epoch"]
+    print "reboot ..."
+    dev.conn._serial.reboot( sync=True )
+        
 if __name__ == '__main__':
 
-	def standalone():
-		if len(sys.argv) == 1:
-			update()
-			print "start tests..."
-			unittest.main()
-		else:
-			print("usage: {0}".format(sys.argv[0]))
-			sys.exit(2)
-		sys.exit(0)
-	standalone()
+    def standalone():
+        if len(sys.argv) == 1:
+            update()
+            print "start tests..."
+            unittest.main()
+        else:
+            print("usage: {0}".format(sys.argv[0]))
+            sys.exit(2)
+        sys.exit(0)
+    standalone()
 
